3aba743d564af8e546070dd01d2d1090e70023f1
javeria2108 <javeriazh@gmail.com>
2024-08-05 23:12:30 +0500

    .

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
   output: "export",
   reactStrictMode: true,
   images: {
+    loader: 'akamai',
+    path: '',
     domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
   },
 }
