ae67a43f9ae5752e137dc8694935c1b4ada5e597
javeria2108 <javeriazh@gmail.com>
2024-08-12 18:51:41 +0500

    analytics added

--- Layout.js
+++ Layout.js
@@ -1,10 +1,11 @@
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 const Layout = ({children, toggleTheme, currentTheme}) => {
     return (
         <div>
             <Navbar toggleTheme={toggleTheme} currentTheme={currentTheme}/>
+            <GoogleAnalytics />
             {children}
             <Footer currentTheme={currentTheme}/>
         </div>
